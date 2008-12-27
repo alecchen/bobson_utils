9da76b0726eb40f13a6ecfbc092c02ae47e06bdc
Alec Chen <alec@cpan.org>
2008-12-27 23:22:37 +0800

    re-organize dir and use hash to store stock info

--- stock/Yahoo/TW/Stock.pm
+++ stock/Yahoo/TW/Stock.pm
@@ -3,19 +3,27 @@ use Mouse;
 use Encode qw(encode decode);
 use WWW::Mechanize;
 use HTML::TableExtract;
-#use Smart::Comments;
+use Smart::Comments;
+use IO::All;
 use version; our $VERSION = qv('0.0.1');
+use Data::TreeDumper;
+
 
 sub fetch {
 	my $self = shift;
 	my $id   = shift;
 
+=pod
 	### fetch
 	my $mech = WWW::Mechanize->new;
 	my $url  = "http://tw.stock.yahoo.com/q/q?s=$id";
 	$mech->get($url);
 	my $content = $mech->content;
 	$content = encode('big5', $content);
+	$content > io('2330.txt');
+=cut
+
+	my $content = io('2330.txt')->slurp;
 
 	### parse
 	my $te = HTML::TableExtract->new;
@@ -26,15 +34,15 @@ sub fetch {
 
 	### elements
 	my @values = @{ $tables[5]->rows->[1] }[0..10];
-	($values[0]) = $values[0] =~ /(\d+)/;
-	$values[5] = sprintf "%2f", $values[2] - $values[7];
+	my @keys   = qw(股票代號 時間 成交 買進 賣出 漲跌 張數 昨收 開盤 最高 最低);
 
-	my %result = (
-		id   => $values[0],
-		nav  => $values[2],
-		dev  => $values[5],
-		date => $date,
-	);
+	$values[0] =~ /(\d+)(\S+)/;
+	$values[0] = $1;
+	my $name   = $2;
+	$values[5] = sprintf "%2f", $values[2] - $values[7];
+	my %result = map { $keys[$_] => $values[$_] } 0..10;
+	$result{'股票名稱'} = $name;
+	$result{'資料日期'} = $date;
 
 	return \%result;
 }
